988dda880f1e95f9a81abaae88ecfc7344c2ac3e
Anirudh Kaushik <anirudhkaushik79@gmail.com>
2018-11-16 14:39:43 +0530

    newsapp2

--- app/src/main/java/com/example/android/newsnano/MainActivity.java
+++ app/src/main/java/com/example/android/newsnano/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.newsnano;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -9,7 +10,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.LoaderManager;
 import android.content.Loader;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
      * URL for News data from Guardian
      */
     private static final String Guardian_REQUEST_URL =
-            "https://content.guardianapis.com/search?api-key=06b02a09-67cf-4ac1-933c-6daf0ba64fb1&show-tags=contributor";
+            "https://content.guardianapis.com/search?q=";
 
     public static final String LOG_TAG = MainActivity.class.getName();
 
@@ -65,7 +69,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         emptyStateTextView = findViewById(R.id.empty_view);
         newsListView.setEmptyView(emptyStateTextView);
 
-
         // Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 
@@ -94,7 +97,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
-        return new NewsLoader(this, Guardian_REQUEST_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String category= sharedPrefs.getString(
+                getString(R.string.settings_category_key), getString(R.string.settings_category_default));
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key), getString(R.string.settings_order_by_default)) ;
+
+        Uri baseUri = Uri.parse(Guardian_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("", category);
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("api-key", "test");
+        uriBuilder.appendQueryParameter("show-tags","contributor");
+
+        return new NewsLoader(this, uriBuilder.toString().replace("&=",""));
     }
 
     @Override
@@ -117,5 +135,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if (news != null && !news.isEmpty()) {
             adapter.addAll(news);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
